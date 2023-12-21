f2cd8acefbf7b9529a03f914e3dbb7c17d504143
A_benaun <Andrey.kuhtin126@gmail.com>
2023-12-21 22:03:01 +0300

    Refactoring code

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,13 @@
 import { Provider } from 'react-redux'
 import '../styles/global.css'
-import store, {persistor} from '../store/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import store from '../store/store'
 
 
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
                 <Component {...pageProps} />
-            </PersistGate>
         </Provider>
     )
 }
